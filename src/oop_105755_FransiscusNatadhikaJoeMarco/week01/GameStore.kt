4fbe60e435642c6ed952c5d6205b50f6be93c4f1
Afterpath <mecharondmarco@gmail.com>
2026-02-04 11:57:23 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_105755_FransiscusNatadhikaJoeMarco/week01/GameStore.kt
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week01/GameStore.kt
@@ -1,9 +1,13 @@
-package oop_105755_FransiscusNatadhikaJoeMarco.week01
-
 fun main() {
     val gameTitle = "Elden Ring"
     val price = 600000
+    val finalPrice = calculateFinalPrice(price)
+
+    printReceipt(title = gameTitle, finalPrice = finalPrice)
 }
 
-fun calculateFinalPrice(price: Int): Int =
-    if (price > 500000) (price * 0.8).toInt() else (price * 0.9).toInt()
+fun printReceipt(title: String, finalPrice: Int) {
+    println("=== Struk Pembelian SteamKW ===")
+    println("Judul Game : $title")
+    println("Harga Akhir: Rp $finalPrice")
+}
