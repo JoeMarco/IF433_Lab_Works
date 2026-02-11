f511e9ae3d0db7dd10d5f56526edbd78e3d64e0e
Afterpath <mecharondmarco@gmail.com>
2026-02-11 11:51:48 +0700

    week02: (task 1) implement library input system

---
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week02/LoanMain.kt
@@ -0,0 +1,30 @@
+package oop_105755_FransiscusNatadhikaJoeMarco.week02
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    println("=== SISTEM PEMINJAMAN BUKU ===")
+
+    print("Masukkan Judul Buku: ")
+    val title = scanner.nextLine()
+
+    print("Masukkan Nama Peminjam: ")
+    val borrower = scanner.nextLine()
+
+    print("Masukkan Lama Pinjam (hari): ")
+    var duration = scanner.nextInt()
+
+    if (duration < 0) {
+        println("WARNING: Durasi tidak boleh minus, diubah menjadi 1 hari.")
+        duration = 1
+    }
+
+    val loan = Loan(title, borrower, duration)
+
+    println("\n--- DETAIL PEMINJAMAN ---")
+    println("Judul Buku    : ${loan.bookTitle}")
+    println("Peminjam      : ${loan.borrower}")
+    println("Durasi        : ${loan.loanDuration} hari")
+    println("Total Denda   : Rp ${loan.calculateFine()}")
+}
