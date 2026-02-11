35fc5e54de15b07a86e6c21cc8748dda5cac3c5c
Afterpath <mecharondmarco@gmail.com>
2026-02-11 11:38:20 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_105755_FransiscusNatadhikaJoeMarco/week02/Student.kt
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week02/Student.kt
@@ -1,18 +1,22 @@
 package oop_105755_FransiscusNatadhikaJoeMarco.week02
 
-class Student (val name: String, val nim: String, var major: String) {
+class Student (
+    val name: String,
+    val nim: String,
+    var gpa: Double = 0.0,  // <-- Default Argument
+    var major: String
+) {
     init {
-        // Validasi Sederhana: Cek panjang NIM
         if (nim.length != 5) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
             println("Data Mahasiswa $name mungkin akan bermasalah di sistem.")
         } else {
             println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
         }
+    }
 
-        constructor(name: String, nim: String) : this(name, nim, major = "Non-Matriculated") {
-            println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
-        }
+    // Secondary Constructor harus di LUAR init block
+    constructor(name: String, nim: String) : this(name, nim, major = "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
     }
 }
-
