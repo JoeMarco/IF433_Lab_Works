b1b34a1d76211b82c082e068b0f5bfbded28088a
Afterpath <mecharondmarco@gmail.com>
2026-02-11 11:49:56 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week02/Loan.kt
@@ -0,0 +1,15 @@
+package oop_105755_FransiscusNatadhikaJoeMarco.week02
+
+class Loan(
+    val bookTitle: String,
+    val borrower: String,
+    val loanDuration: Int = 1  // Default Argument = 1 hari
+) {
+    fun calculateFine(): Int {
+        return if (loanDuration > 3) {
+            (loanDuration - 3) * 2000
+        } else {
+            0
+        }
+    }
+}
