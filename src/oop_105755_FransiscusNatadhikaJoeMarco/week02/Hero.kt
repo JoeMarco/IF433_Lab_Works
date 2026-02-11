f4441f8017c27a41fd6becb7692df540ce01842c
Afterpath <mecharondmarco@gmail.com>
2026-02-11 11:57:23 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week02/Hero.kt
@@ -0,0 +1,22 @@
+package oop_105755_FransiscusNatadhikaJoeMarco.week02
+
+class Hero(
+    val name: String,
+    val baseDamage: Int,
+    var hp: Int = 100  // Default Argument = 100 HP
+) {
+    fun attack(targetName: String) {
+        println("$name menebas $targetName!")
+    }
+
+    fun takeDamage(damage: Int) {
+        hp -= damage
+        if (hp < 0) {
+            hp = 0
+        }
+    }
+
+    fun isAlive(): Boolean {
+        return hp > 0
+    }
+}
