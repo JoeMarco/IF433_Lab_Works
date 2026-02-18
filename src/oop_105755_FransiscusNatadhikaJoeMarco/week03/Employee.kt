a4eddec67e2e08c4e88bf79b50c143e7b7a19622
Afterpath <mecharondmarco@gmail.com>
2026-02-18 10:27:42 +0700

    week03: simulate stack overflow error in setter

--- src/oop_105755_FransiscusNatadhikaJoeMarco/week03/Employee.kt
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week03/Employee.kt
@@ -2,4 +2,8 @@ package oop_105755_FransiscusNatadhikaJoeMarco.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba Set Gaji Ke: $value")
+            this.salary = value
+        }
 }
---
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_105755_FransiscusNatadhikaJoeMarco.week03
+
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
