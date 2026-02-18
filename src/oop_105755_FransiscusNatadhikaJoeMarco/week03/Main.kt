e7ffc0ec723e9d78176b9221000d30f291a86c23
Afterpath <mecharondmarco@gmail.com>
2026-02-18 10:35:36 +0700

    week03: test all encapsulation logic in main

--- src/oop_105755_FransiscusNatadhikaJoeMarco/week03/Main.kt
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week03/Main.kt
@@ -2,6 +2,10 @@ package oop_105755_FransiscusNatadhikaJoeMarco.week03
 
 fun main() {
     val e = Employee("Budi")
-    println("--- Test Error ---")
-    e.salary = 5000
+    e.salary = -1000
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
+
+    e.increasePerformance()
+    println("Pajak yang Harus Dibayar: ${e.tax}")
 }
