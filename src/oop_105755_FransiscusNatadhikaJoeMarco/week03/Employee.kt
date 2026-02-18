75423ba665daf42801629b66bb0e14b813f95b3c
Afterpath <mecharondmarco@gmail.com>
2026-02-18 10:32:35 +0700

    week03: add private property

--- src/oop_105755_FransiscusNatadhikaJoeMarco/week03/Employee.kt
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week03/Employee.kt
@@ -10,4 +10,15 @@ class Employee(val name: String) {
                 field = value
             }
         }
+
+    private var performanceRating: Int = 3
+
+    fun increasePerformance() {
+        performanceRating++
+        println("Kinerja $name Meningkat! Rating: $performanceRating")
+    }
+
+    fun printStatus() {
+        println("Karyawan: $name, Rating: $performanceRating")
+    }
 }
