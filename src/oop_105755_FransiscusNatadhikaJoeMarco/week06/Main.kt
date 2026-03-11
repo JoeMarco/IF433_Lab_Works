736f4414f8b0a5fae5801df986d0630c124204ba
Afterpath <mecharondmarco@gmail.com>
2026-03-11 11:05:59 +0700

    week06: (task) instantiate smart devices in main

--- src/oop_105755_FransiscusNatadhikaJoeMarco/week06/Main.kt
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week06/Main.kt
@@ -1,22 +1,7 @@
 package oop_105755_FransiscusNatadhikaJoeMarco.week06
 
-fun processCheckout(method: PaymentMethod, amount: Double) {
-    print("-> Memulai checkout...")
-    method.pay(amount)
-}
-
 fun main() {
-    val myWatch = Smartwatch()
-    myWatch.showTime()
-
-    val myPhone = Smartphone()
-    myPhone.turnOn()
-
-    val pay1 = Gopay()
-    val pay2 = CreditCard()
-
-    print("\n=== TESTING CHECKOUT ===")
-    processCheckout(method = pay1, amount = 50000.0)
-    processCheckout(method = pay2, amount = 150000.0)
-    )
+    val lamp    = SmartLamp(id = "LAMP-01",  name = "Ruang Tamu")
+    val speaker = SmartSpeaker(id = "SPK-01", name = "Google Nest Dapur")
+    val cctv    = SmartCCTV(id = "CCTV-01",  name = "Ezviz Garasi")
 }
