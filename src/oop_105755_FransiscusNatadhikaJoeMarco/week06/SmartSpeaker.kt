3b2427f51b70a6ea7319f0148b3fb5777342dd31
Afterpath <mecharondmarco@gmail.com>
2026-03-11 10:59:20 +0700

    week06: (task) create SmartSpeaker

--- src/oop_105755_FransiscusNatadhikaJoeMarco/week06/SmartSpeaker.kt
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week06/SmartSpeaker.kt
@@ -1,15 +1,19 @@
 package oop_105755_FransiscusNatadhikaJoeMarco.week06
 
-class SmartLamp(
+class SmartSpeaker(
     override val id: String,
     override val name: String
 ) : SmartDevice, Switchable {
 
     override fun turnOn() {
-        println("[$name] Lampu menyala dengan cahaya hangat")
+        println("[$name] Speaker menyala dan siap digunakan.")
     }
 
     override fun turnOff() {
-        println("[$name] Lampu dimatikan")
+        println("[$name] Speaker dimatikan.")
+    }
+
+    fun playMusic(song: String) {
+        println("[$name] Memutar lagu $song dari Spotify.")
     }
 }
