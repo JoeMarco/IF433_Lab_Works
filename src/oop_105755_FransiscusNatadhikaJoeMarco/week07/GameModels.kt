0fa387e888c85d4a380bf0926c8ec846af7ba163
Afterpath <mecharondmarco@gmail.com>
2026-03-25 10:48:20 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_105755_FransiscusNatadhikaJoeMarco.week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(40),
+    RARE(20),
+    EPIC(5),
+    LEGENDARY(1);
+}
