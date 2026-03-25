e66203abe9fa45a3f9e54749e46b4a048736909f
Afterpath <mecharondmarco@gmail.com>
2026-03-25 10:43:20 +0700

    week07: (task) create GameManager singleton

---
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week07/GameManager.kt
@@ -0,0 +1,15 @@
+package oop_105755_FransiscusNatadhikaJoeMarco.week07
+
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
+
