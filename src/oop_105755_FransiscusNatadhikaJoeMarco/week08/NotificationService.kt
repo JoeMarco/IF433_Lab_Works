0d556f342ad77706da7f83fc1f875be662bee421
Afterpath <mecharondmarco@gmail.com>
2026-04-15 09:43:55 +0700

    week08: implement smart cast in function

---
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week08/NotificationService.kt
@@ -0,0 +1,15 @@
+package oop_105755_FransiscusNatadhikaJoeMarco.week08
+
+class NotificationService {
+    fun sendEmail(emailAddress: String) {
+        println("Mengirim email ke: $emailAddress")
+    }
+
+    fun processUser(user: UserProfile) {
+        if (user.email != null) {
+            sendEmail(emailAddress = user.email)
+        } else {
+            println("User ${user.name} tidak memiliki email.")
+        }
+    }
+}
