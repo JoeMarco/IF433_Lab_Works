3ca5877da6e98f794afb1b5d1d6a15da34b09169
Afterpath <mecharondmarco@gmail.com>
2026-04-15 09:42:45 +0700

    week08: create basic UserProfile with nullable fields

---
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week08/UserProfile.kt
@@ -0,0 +1,7 @@
+package oop_105755_FransiscusNatadhikaJoeMarco.week08
+
+class UserProfile(
+    val name: String,
+    val email: String?,
+    val phone: String? = null // Default argument null
+)
