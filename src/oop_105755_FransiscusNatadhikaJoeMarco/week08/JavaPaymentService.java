acbed6f51487034f42c19a6b112d453efd3c9cc0
Afterpath <mecharondmarco@gmail.com>
2026-04-15 10:17:17 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_105755_FransiscusNatadhikaJoeMarco.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
