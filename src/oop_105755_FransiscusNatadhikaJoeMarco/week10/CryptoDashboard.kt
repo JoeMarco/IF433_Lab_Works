257278d43d00d19f59a422e775b7541f594640d6
Afterpath <mecharondmarco@gmail.com>
2026-04-29 09:44:28 +0700

    week10: (task) populate coin repository

--- src/oop_105755_FransiscusNatadhikaJoeMarco/week10/CryptoDashboard.kt
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week10/CryptoDashboard.kt
@@ -2,3 +2,6 @@ package oop_105755_FransiscusNatadhikaJoeMarco.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC",  0.85))
+    coinRepo.add(Coin("ETH",  5.20))
+    coinRepo.add(Coin("USDT", 1500.00))
