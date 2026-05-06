3df9202a8ee3ccf601dd5cfc4591b0256407b318
Afterpath <mecharondmarco@gmail.com>
2026-05-06 10:50:45 +0700

    week11: create string extension functions

---
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week11/StringExtensions.kt
@@ -0,0 +1,9 @@
+package oop_105755_FransiscusNatadhikaJoeMarco.week11
+
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
