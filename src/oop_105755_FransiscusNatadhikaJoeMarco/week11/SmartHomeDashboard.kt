c0b2e16cdf0b320ce597551fe2852eee706b9440
Afterpath <mecharondmarco@gmail.com>
2026-05-06 11:01:31 +0700

    week11: (task) implement device search with let

--- src/oop_105755_FransiscusNatadhikaJoeMarco/week11/SmartHomeDashboard.kt
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week11/SmartHomeDashboard.kt
@@ -23,4 +23,10 @@ fun main() {
     }
     homeDevices.add(ac)
     homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
+
+    val searchResult = homeDevices.find { it.category == "Camera" }
+    searchResult?.let {
+        println("\n=== HASIL PENCARIAN KAMERA ===")
+        println(it.diagnose())
+    }
 }
