190763fbbe69332711969dcaba81527a8db43c15
Afterpath <mecharondmarco@gmail.com>
2026-05-13 10:42:31 +0700

    week12: implement basic runCatching

---
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week12/RunCatchingExample.kt
@@ -0,0 +1,7 @@
+package oop_105755_FransiscusNatadhikaJoeMarco.week12
+
+fun main() {
+    println("=== TEST RUNCATCHING ===")
+    val result: Result<Int> = runCatching {
+        "42X".toInt()
+    }
