f008340a12015b197c70ffd802491e78c9f7a7cf
Afterpath <mecharondmarco@gmail.com>
2026-05-13 10:44:51 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week12/SmartFeeder.kt
@@ -0,0 +1,4 @@
+package oop_105755_FransiscusNatadhikaJoeMarco.week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
