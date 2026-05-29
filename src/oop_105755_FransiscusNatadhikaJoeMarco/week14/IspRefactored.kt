40608f694870ec634e6a151d91893a714bf0c638
Afterpath <mecharondmarco@gmail.com>
2026-05-29 15:24:09 +0700

    week14: refactor SimplePrinter to implement only Printable

--- src/oop_105755_FransiscusNatadhikaJoeMarco/week14/IspRefactored.kt
+++ src/oop_105755_FransiscusNatadhikaJoeMarco/week14/IspRefactored.kt
@@ -11,3 +11,13 @@ interface Scannable {
 interface Faxable {
     fun fax(doc: String)
 }
+
+class ModernSimplePrinter : Printable {
+    override fun print(doc: String) = println("Printing securely: $doc")
+}
+
+class AllInOnePrinter : Printable, Scannable, Faxable {
+    override fun print(doc: String) = println("Printing: $doc")
+    override fun scan(doc: String): String = "Scanned: $doc"
+    override fun fax(doc: String) = println("Faxing: $doc")
+}
